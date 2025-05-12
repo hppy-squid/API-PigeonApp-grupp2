33c5b65503a30d6654b951cc82a3c6053f5deb05
Felicia Karlsson <felizia02@icloud.com>
2025-05-12 14:13:01 +0200

    spring boot projekt startad

---
+++ PigeonApp/src/main/java/PigeonApp/PigeonApp/PigeonAppApplication.java
@@ -0,0 +1,13 @@
+package PigeonApp.PigeonApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonAppApplication.class, args);
+	}
+
+}
---
+++ PigeonApp/src/test/java/PigeonApp/PigeonApp/PigeonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package PigeonApp.PigeonApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
