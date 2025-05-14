d74f10702ec36a5feb9487816c3dbcfa3a107348
Martin Bjervig <martinbjervig@gmail.com>
2025-05-14 14:17:32 +0200

    added dotenv + moved files + DB connection

--- src/main/java/PigeonApp/PigeonApp/PigeonAppApplication.java
+++ src/main/java/PigeonApp/PigeonApp/PigeonAppApplication.java
@@ -3,10 +3,15 @@ package PigeonApp.PigeonApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class PigeonAppApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SPRING_DATA_MONGODB_URI", dotenv.get("SPRING_DATA_MONGODB_URI"));
+
 		SpringApplication.run(PigeonAppApplication.class, args);
 	}
 
