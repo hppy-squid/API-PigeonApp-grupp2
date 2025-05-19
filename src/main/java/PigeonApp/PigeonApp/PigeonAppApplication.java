872f77b0639d565fd17cc563d06519d22bfd8724
Martin Bjervig <martinbjervig@gmail.com>
2025-05-19 11:24:00 +0200

    added Dockerfile + removed dotenv dependency, redy to deploy

--- src/main/java/PigeonApp/PigeonApp/PigeonAppApplication.java
+++ src/main/java/PigeonApp/PigeonApp/PigeonAppApplication.java
@@ -3,16 +3,10 @@ package PigeonApp.PigeonApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class PigeonAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Working Directory = " + System.getProperty("user.dir"));
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("SPRING_DATA_MONGODB_URI", dotenv.get("SPRING_DATA_MONGODB_URI"));
 
 		SpringApplication.run(PigeonAppApplication.class, args);
 	}
